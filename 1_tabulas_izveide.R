ee2fda96df6ae569e6029e1f4fb5e6038f599739
Martins Locs <locs.martins@gmail.com>
2025-04-14 10:38:12 +0300

    Precizējumi

--- 1_tabulas_izveide.R
+++ 1_tabulas_izveide.R
@@ -1,3 +1,4 @@
+# 1. Bibliotēku ielāde ----
 library(readxl)
 library(plyr)
 library(dplyr)
@@ -9,15 +10,11 @@ library(psych)
 
 
 
-# 1. Darbs ar XML failu ----
-## 1.1. Ielādē XML failu ----
+# 2. Datu iegūšana no direktīvas XML ----
+
 xml_data <- xmlParse("./IevadesDati/LV_birds_reports_20191030-151740.xml")  
 xml_list <- xmlToList(xml_data)
-
-## 1.2. Filtrējam XML sarakstu, lai saglabājam tikai 'bird_report' elementus
 xml_list <- xml_list[names(xml_list) == "bird_report"]
-
-## 1.3. Pārvērš XML par datu rāmi, pielāgojot atšķirīgu kolonnu skaitu ----
 xml_dati <- ldply(xml_list, function(x) as.data.frame(t(unlist(x)), stringsAsFactors = FALSE))
 
 xml_dati <- xml_dati %>%
@@ -28,14 +25,14 @@ xml_dati <- xml_dati %>%
            if (length(x) == 3) paste(x[1], x[2]) else paste(x, collapse = " ")
          }))
 
-## 1.4. Saglabā Excel failā ----
 write.xlsx(xml_dati, "./IevadesDati/LV_birds_reports_2019.xlsx")
 
 
 
 
-#2. Datu apvienošana ----
-## 2.1. Ielādē failus ----
+# 3. Datu apvienošana ----
+
+## 3.1. Datu ielāde ----
 putni_direktiva <- read_excel("./IevadesDati/LV_birds_reports_2019.xlsx")
 putni_dabasdati <- read_excel("./IevadesDati/putni_SkaitiPazimes_kops2016.xlsx", .name_repair = "universal")
 
@@ -51,7 +48,7 @@ putni_dabasdati <- putni_dabasdati %>%
            !str_detect(sugaZIN, " sp\\.") & !str_detect(sugaZIN, "/"))
 
 
-## 2.2. Populāciju īstermiņa aprēķins
+## 3.2. Populācijas vērtību apstrāde un aprēķins
 putni_direktiva$population_size_min <- as.numeric(putni_direktiva$population_size_min)
 putni_direktiva$population_size_max <- as.numeric(putni_direktiva$population_size_max)
 putni_direktiva$population_size <- as.numeric(putni_direktiva$population_size)
@@ -74,9 +71,7 @@ putni_direktiva$population_size <- ifelse(
 )
 
 
-
-
-## 2.2. Pārsauc kolonnu nosaukumus un izrēķina dziedātāju attiecību ----
+## 3.3. Pārsauc kolonnu nosaukumus un izrēķina dziedātāju attiecību ----
 putni_dabasdati <- putni_dabasdati %>%
   rename_with(~ c("common_speciesname", "speciesname")[match(.x, c("sugaLV", "sugaZIN"))], 
               .cols = c("sugaLV", "sugaZIN"))
@@ -91,7 +86,6 @@ putni_dabasdati <- putni_dabasdati %>%
     TRUE ~ NA_character_
   ))
 
-
 putni_dabasdati$Dzied.Biotopa.attieciba <- ifelse(
   putni_dabasdati$SkaitsBiotopa == 0, NA, 
   putni_dabasdati$SkaitsDzied / putni_dabasdati$SkaitsBiotopa
@@ -101,9 +95,7 @@ putni_dabasdati$Dzied.Biotopa.attieciba <- gsub(",", ".", putni_dabasdati$Dzied.
 putni_dabasdati$Dzied.Biotopa.attieciba <- as.numeric(putni_dabasdati$Dzied.Biotopa.attieciba)
 
 
-
-
-## 2.3. Pārbauda un apvieno duplikātus, kā prioritāti ņemot ligzdotājus ----
+## 3.4. Pārbauda un apvieno duplikātus, kā prioritāti ņemot ligzdotājus ----
 cat("Dublikātu skaits putni_direktiva (common_speciesname):", sum(duplicated(putni_direktiva$common_speciesname)), "\n")
 cat("Dublikātu skaits putni_dabasdati (common_speciesname):", sum(duplicated(putni_dabasdati$common_speciesname)), "\n")
 
@@ -117,15 +109,13 @@ putni_direktiva_clean <- putni_direktiva %>%
   ungroup()
 
 
-## 2.4. Atlasa monitoringa robus ----
+## 3.5. Monitoringa nepilnību atlase ----
 putni_direktiva_clean <- putni_direktiva_clean %>%
   mutate(nepilnigie_monitoringi = ifelse(population_method.text %in% c("estimateExpert", "estimatePartial"), 1, 0)) %>%
   relocate(nepilnigie_monitoringi, .after = population_method.text)
 
 
-
-
-## 2.5. Apvieno datus ----
+## 3.6. Datu apvienošana ----
 apvienots_1 <- putni_dabasdati %>%
   left_join(putni_direktiva_clean, by = c("common_speciesname", "speciesname"))
 
@@ -135,7 +125,7 @@ apvienots_1 <- apvienots_1 %>%
 
 
 
-## 3. Ielādē preferences.xlsx un pārsauc kolonnu nosaukumus ----
+# 4. Ielādē preferences un pārsauc kolonnu nosaukumus ----
 preferences <- read_excel("./IevadesDati/preferences.xlsx", .name_repair = "universal")
 
 preferences <- preferences %>%
@@ -150,17 +140,15 @@ preferences <- preferences %>%
            !str_detect(sugaZIN, " sp\\.") & !str_detect(sugaZIN, "/"))
 
 
-
-# 3.1. Pārsauc kolonnu nosaukumus, ja tās pastāv
+## 4.1. Kolonu nosaukumu pārsaukšana ----
 preferences <- preferences %>%
   rename_with(~ c("common_speciesname", "speciesname")[match(.x, c("sugaLV", "sugaZIN"))], 
               .cols = c("sugaLV", "sugaZIN"))
 
-
 cols_to_duplicate <- setdiff(names(preferences), c("common_speciesname", "speciesname"))
 
 
-## 3.2. Apvieno preferences.xlsx ar esošo tabulu ----
+## 4.2. Preferenču apvienošana ar esošo tabulu ----
 apvienots_2 <- apvienots_1 %>%
   full_join(preferences, by = c("common_speciesname", "speciesname"))
 
@@ -168,5 +156,7 @@ apvienots_2 <- apvienots_2 %>%
   filter(!str_detect(speciesname, "sp\\.|/"))
 
 
-## 4. Saglabā rezultātu ----
+
+
+# 5. Rezultātu saglabāšana ----
 #write.xlsx(apvienots_2, "./IevadesDati/putni.xlsx", overwrite = TRUE)
